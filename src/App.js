d6cbdf9196c9adfdae32bf6d1292b5b79d006a92
Shinazm <mohammad.shinaz@nineleaps.com>
2021-01-23 23:11:36 +0530

    created menu bar drawer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React,{useState} from 'react';
 import './App.css';
 import Header from './Header'
 import { BrowserRouter, Switch,Route,Redirect} from 'react-router-dom'
+import Menu from './Menu'
 
 function App() {
   const [isMenuOpen, setIsMenuOpen] = useState(false)
@@ -10,6 +11,7 @@ function App() {
     <div className="app">
       <Header isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen}/>
       </div> 
+      {isMenuOpen&& <Menu/>}  
     </BrowserRouter>
   );
 }
